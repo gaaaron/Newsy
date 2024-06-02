1aa7760c21bfd13c79d9dd448622594bf2da3f0d
gaaaron <garancsya@gmail.com>
2024-06-02 12:37:02 +0200

    Side-menu created

--- Newsy.Client/src/app/app.component.ts
+++ Newsy.Client/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, TemplateRef } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet, RouterLink, DashboardComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private offcanvasService = inject(NgbOffcanvas);
   title = 'Newsy.Client';
+
+  open(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' });
+	}
+
 }
--- Newsy.Client/src/app/app.routes.ts
+++ Newsy.Client/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SourcesComponent } from './sources/sources.component';
+import { TagsComponent } from './tags/tags.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: DashboardComponent,
+    title: 'Dashboard page',
+}, {
+    path: 'sources',
+    component: SourcesComponent,
+    title: 'Sources page',
+}, {
+    path: 'tags',
+    component: TagsComponent,
+    title: 'Tags page',
+}];
